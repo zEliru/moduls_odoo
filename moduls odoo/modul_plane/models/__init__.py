80de5f2e1f039bc3d7189033ddf7cb24b88c6307
zEliru <123090657+zEliru@users.noreply.github.com>
2023-05-08 20:53:30 +0200

    He actualitzat els .py
    En principi per ara ho he fet tot, cal tocar el MANIFEST next.

---
+++ moduls odoo/modul_plane/models/__init__.py
@@ -0,0 +1,5 @@
+from . import plane_aeroport
+from . import plane_avio
+from . import plane_pilot
+from . import plane_vol
+
---
+++ moduls odoo/modul_zoo/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Modul Zoo",
+    'summary':"El meu modul de zoo",
+    'description':"Doncs bé, aquest ve a ser el meu mòdul de Zoo. Sóc l'Edu, per cert.",
+    'author':"Edu Juvé Vidal",
+    'license':"AGPL-3",
+    'website': "https://www.op.gg/summoners/euw/edu",
+    'category':"Uncategorized",
+    'version': "14.0",
+    'data':"['security/ir.model.access.csv','views/views.xml']",
+
+}
