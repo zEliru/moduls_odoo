f4f2ecc9a24eede0dded879abe3ec2d7a8503237
zEliru <123090657+zEliru@users.noreply.github.com>
2023-05-11 16:18:02 +0200

    Delete moduls odoo directory

--- moduls odoo/modul_plane/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import plane_aeroport
-from . import plane_avio
-from . import plane_pilot
-from . import plane_vol
-
--- moduls odoo/modul_zoo/models/zoo_especie.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-class zoo_especie(models.Model):
-    _name = 'zoo.especie'
-    familia = fields.Char('Familia')
-    nomCientific = fields.Char('Nom científic')
-    nomVulgar = fields.Char('Nom vulgar')
-    perill = fields.Char('Perill')
-
-    
