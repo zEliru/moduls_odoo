251262d1ef3872b3fa8010fe85a366f526fb2574
zEliru <123090657+zEliru@users.noreply.github.com>
2023-05-15 19:52:30 +0200

    Update plane_avio.py

--- modul_plane/models/plane_avio.py
+++ modul_plane/models/plane_avio.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 class plane_avio(models.Model):
         _name = 'plane.avio'
         codi = fields.Integer('Codi')
-        imatge = fields.Char('Imatge')
+        imatge = fields.Char('Imatge') # imatge o binary sus
         marca = fields.Integer('Marca')
         model = fields.Char('Model')
         maxVel = fields.Float('Velocitat màxima')
