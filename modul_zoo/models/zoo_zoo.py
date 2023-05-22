de3d07af5b773fdd4c781949bcfd9f28152b8874
zEliru <123090657+zEliru@users.noreply.github.com>
2023-05-22 11:15:56 +0200

    Delete modul_zoo directory
    deleting because of a new version

--- modul_zoo/models/zoo_zoo.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-class zoo_especie(models.Model):
-    _name = 'zoo.zoo'
-    grandaria = fields.Integer('Grandària')
-    nom = fields.Char('Nom')
-    ciutat = fields.Char('Nom de la ciutat')
-    Pais = fields.Char('Pais')
-
-    
