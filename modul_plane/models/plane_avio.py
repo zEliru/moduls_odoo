4c6191a1d6538fe4001731af4b8a7af85dc58895
zEliru <123090657+zEliru@users.noreply.github.com>
2023-05-22 11:16:14 +0200

    Delete modul_plane directory
    deleting due to an update

--- modul_plane/models/plane_avio.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-class plane_avio(models.Model):
-        _name = 'plane.avio'
-        codi = fields.Integer('Codi')
-        imatge = fields.Char('Imatge')
-        marca = fields.Integer('Marca')
-        model = fields.Char('Model')
-        maxVel = fields.Float('Velocitat màxima')
-        
